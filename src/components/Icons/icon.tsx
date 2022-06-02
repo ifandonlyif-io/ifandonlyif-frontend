d2ecd852a5b0834b3e48502fa1c5666b9ca56312
Garfield550 <openpgp@550.moe>
2022-06-02 11:49:41 +0000

    feat: add icon components

---
+++ src/components/Icons/icon.tsx
@@ -0,0 +1,9 @@
+import { SvgIconProps, SvgIcons } from './svgIcon'
+
+export function SelectMenuArrowIcon(props: SvgIconProps) {
+  return (
+    <SvgIcons viewBox="0 0 16 11" {...props}>
+      <path d="M8.74193 10.353C8.34159 10.855 7.57866 10.855 7.17832 10.353L0.481945 1.95678C-0.040451 1.30177 0.425938 0.333252 1.26375 0.333252H14.6565C15.4943 0.333252 15.9607 1.30177 15.4383 1.95678L8.74193 10.353Z" />
+    </SvgIcons>
+  )
+}
---
+++ src/components/Icons/svgIcon.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { BaseComponent } from 'types'
+import { classNames } from 'utils'
+
+export type SvgIconProps = BaseComponent &
+  React.SVGProps<SVGSVGElement> & {
+    /**
+     * Node passed into the SVG element.
+     */
+    children?: React.ReactNode
+    /**
+     * The fontSize applied to the icon. Defaults to 24px, but can be configure to inherit font size.
+     * @default 24
+     */
+    fontSize?: 'inherit' | number
+    /**
+     * Applies a color attribute to the SVG element.
+     * @default 'inherit'
+     */
+    htmlColor?: string
+    /**
+     * The shape-rendering attribute. The behavior of the different options is described on the
+     * [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/shape-rendering).
+     * If you are having issues with blurry icons you should investigate this prop.
+     */
+    shapeRendering?: string
+    /**
+     * Provides a human-readable title for the element that contains it.
+     * https://www.w3.org/TR/SVG-access/#Equivalent
+     */
+    titleAccess?: string
+    /**
+     * Allows you to redefine what the coordinates without units mean inside an SVG element.
+     * For example, if the SVG element is 500 (width) by 200 (height),
+     * and you pass viewBox="0 0 50 20",
+     * this means that the coordinates inside the SVG will go from the top left corner (0,0)
+     * to bottom right (50,20) and each unit will be worth 10px.
+     * @default '0 0 24 24'
+     */
+    viewBox?: string
+  }
+
+export function SvgIcons({
+  children,
+  className,
+  htmlColor,
+  titleAccess,
+  fontSize = 24,
+  viewBox = '0 0 24 24',
+  ...props
+}: SvgIconProps) {
+  return (
+    <svg
+      className={classNames(
+        'inline-block shrink-0 select-none',
+        'w-[1em] h-[1em] fill-current text-2xl leading-none',
+        className
+      )}
+      focusable="false"
+      style={{
+        color: htmlColor || 'inherit',
+        fontSize: fontSize === 'inherit' ? 'inherit' : `${fontSize}px`,
+      }}
+      viewBox={viewBox}
+      aria-hidden={titleAccess ? undefined : true}
+      role={titleAccess ? 'img' : undefined}
+      {...props}
+    >
+      {children}
+      {titleAccess ? <title>{titleAccess}</title> : null}
+    </svg>
+  )
+}
