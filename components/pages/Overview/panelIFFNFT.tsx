b8072c35e661f9ed38c61a273971ea4e51f57cf2
Garfield550 <openpgp@550.moe>
2022-06-25 10:04:24 +0000

    feat: update tab panels

--- components/pages/Overview/panelIFFNFT.tsx
+++ components/pages/Overview/panelIFFNFT.tsx
@@ -1,10 +1,14 @@
-import { TabTitle } from './title'
+import { SectionTitleWithSortTimezone, TabTitle } from './title'
 
 export function PanelIFFNFT() {
   return (
-    <div className="py-[50px] px-5">
+    <div className="py-[50px] px-5 min-h-[640px]">
       <TabTitle className="mb-4">MY IFFNFT</TabTitle>
-      <section className="flex flex-row gap-14"> </section>
+      <SectionTitleWithSortTimezone
+        className="mb-4"
+        onOptionChange={() => void 0}
+      />
+      <section className="flex flex-col"></section>
     </div>
   )
 }
--- components/pages/Overview/panelPreMint.tsx
+++ components/pages/Overview/panelPreMint.tsx
@@ -1,10 +1,18 @@
-import { TabTitle } from './title'
+import { SectionTitle, SectionTitleWithSortTimezone, TabTitle } from './title'
 
 export function PanelPreMint() {
   return (
-    <div className="py-[50px] px-5">
+    <div className="py-[50px] px-5 min-h-[640px]">
       <TabTitle className="mb-4">Pre-mint NFT</TabTitle>
-      <section className="flex flex-row gap-14"> </section>
+      <SectionTitleWithSortTimezone
+        className="mb-4"
+        onOptionChange={() => void 0}
+      />
+      <section className="flex flex-col">
+        <SectionTitle className="mb-[10px] uppercase" size="small" count={5}>
+          MY WHITELIST
+        </SectionTitle>
+      </section>
     </div>
   )
 }
