567a0533bf4f886ec3171b4e2c0bdcb9fae7ce43
Garfield550 <openpgp@550.moe>
2022-06-29 12:16:55 +0000

    feat: support filter nft section

--- components/pages/Overview/panelMintIt.tsx
+++ components/pages/Overview/panelMintIt.tsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import { classNames } from 'utils'
+
 import { FilterGroup, FilterItem } from './filter'
 import { MintItMyNFT, MintItMyNFTProps } from './mintItMyNFT'
 import { MintItWhitelist, MintItWhitelistProps } from './mintItWhitelist'
@@ -5,24 +8,51 @@ import { TabTitle } from './title'
 
 export type PanelMintItProps = MintItWhitelistProps & MintItMyNFTProps
 
+type FilterValues = 'all' | 'whitelist' | 'nft'
+
+type FilterOption = { label: string; value: FilterValues; count: number }
+
 export function PanelMintIt(props: PanelMintItProps) {
+  const { myWhitelist, preSaleWhitelist, myNFT } = props
+  const whitelistCount = myWhitelist.length + preSaleWhitelist.length
+  const allCount = whitelistCount + myNFT.length
+  const filterItems: FilterOption[] = [
+    { label: 'ALL', value: 'all', count: allCount },
+    { label: 'WHITELIST', value: 'whitelist', count: whitelistCount },
+    { label: 'NFT', value: 'nft', count: myNFT.length },
+  ]
+  const [mintFilter, setMintFilter] = React.useState<string>('all')
+  const handleFilterChange = React.useCallback(
+    (value: string) => setMintFilter(value),
+    []
+  )
+  const isShowAll = mintFilter === 'all'
+  const isShowWhitelist = isShowAll || mintFilter === 'whitelist'
+  const isShowMy = isShowAll || mintFilter === 'nft'
   return (
     <div className="py-[50px] px-5">
       <TabTitle className="mb-4">All You Can Mint</TabTitle>
-      <FilterGroup className="mb-7" name="nftType">
-        <FilterItem value="all" count={15}>
-          ALL
-        </FilterItem>
-        <FilterItem value="whitelist" count={11}>
-          WHITELIST
-        </FilterItem>
-        <FilterItem value="nft" count={4}>
-          NFT
-        </FilterItem>
+      <FilterGroup
+        className="mb-7"
+        name="nftType"
+        defaultValue={mintFilter}
+        onFilterChange={handleFilterChange}
+      >
+        {filterItems.map((item) => (
+          <FilterItem value={item.value} count={item.count} key={item.value}>
+            {item.label}
+          </FilterItem>
+        ))}
       </FilterGroup>
       <div className="flex flex-col gap-28">
-        <MintItWhitelist {...props} />
-        <MintItMyNFT {...props} />
+        <MintItWhitelist
+          className={classNames(isShowWhitelist ? 'flex' : 'hidden')}
+          {...props}
+        />
+        <MintItMyNFT
+          className={classNames(isShowMy ? 'flex' : 'hidden')}
+          {...props}
+        />
       </div>
     </div>
   )
