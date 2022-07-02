56e5b74e473493d5af566a35400a3bd647ec1d01
Garfield550 <openpgp@550.moe>
2022-07-02 20:57:43 +0000

    feat: add nft utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,5 @@
 export * from './datetime'
+export * from './nft'
 
 export function classNames(...classes: unknown[]): string {
   return classes.filter(Boolean).join(' ')
---
+++ utils/nft.ts
@@ -0,0 +1,22 @@
+import { NFTItem } from 'types'
+
+import { isHistorical } from './datetime'
+
+export function sortNFTItems(items: NFTItem[]): NFTItem[] {
+  return items.sort((l, r) => l.unixEpoch - r.unixEpoch)
+}
+
+export function filterExpired(items: NFTItem[]): NFTItem[] {
+  return items.filter((item) => isHistorical(item.unixEpoch))
+}
+
+type FilteredNFTItems = {
+  availableNFTs: NFTItem[]
+  expiredNFTs: NFTItem[]
+}
+export function filteredNFTItems(items: NFTItem[]): FilteredNFTItems {
+  const sortedNFTs = sortNFTItems(items)
+  const expiredNFTs = filterExpired(sortedNFTs).reverse()
+  const availableNFTs = sortedNFTs.filter((nft) => !expiredNFTs.includes(nft))
+  return { availableNFTs, expiredNFTs }
+}
