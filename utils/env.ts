6d201ecaed448c91d28763fbc50f8bcac140dc61
Garfield550 <Garfield550@users.noreply.github.com>
2023-01-10 13:55:38 +0000

    feat: add new env

---
+++ utils/env.ts
@@ -0,0 +1,19 @@
+export function getInfuraApiKey(): string {
+  const apiKey: string | undefined = process.env.NEXT_PUBLIC_INFURA_API_KEY
+  if (!apiKey) throw new TypeError('NEXT_PUBLIC_INFURA_API_KEY not set')
+  return apiKey
+}
+
+export function getDefaultChainId(): string {
+  const chainId: string | undefined = process.env.NEXT_PUBLIC_CHAIN_ID
+  if (!chainId) throw new TypeError('NEXT_PUBLIC_CHAIN_ID not set')
+  return chainId
+}
+
+export function getIffNftContractAddress(): string {
+  const address: string | undefined =
+    process.env.NEXT_PUBLIC_IFFNFT_CONTRACT_ADDRESS
+  if (!address)
+    throw new TypeError('NEXT_PUBLIC_IFFNFT_CONTRACT_ADDRESS not set')
+  return address
+}
