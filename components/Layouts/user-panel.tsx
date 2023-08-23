4f185715a0aed4b532b989a640d7aabde5201dfc
Garfield Lee <Garfield550@users.noreply.github.com>
2023-08-23 15:07:00 +0800

    fix: lint issue

--- components/Layouts/user-panel.tsx
+++ components/Layouts/user-panel.tsx
@@ -21,8 +21,8 @@ function WalletDropdown(properties: WalletDropdownProperties) {
   const { account, signOut } = useIffAccount()
   const accountString = account && shortenAddress(account.wallet)
 
-  const handleDisconnectClick = React.useCallback(async () => {
-    await signOut()
+  const handleDisconnectClick = React.useCallback(() => {
+    signOut()
   }, [signOut])
 
   return (
